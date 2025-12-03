685f3e0e39ecc418d27a687cc3dd77cd89e8cbc0
Ibrahima Gueye <eb091941@ens.uqam.ca>
2025-12-03 15:19:04 -0500

    Ajout de ddetection Couleur , suite, carre et quinte

--- src/main/java/org/example/MainJoueur.java
+++ src/main/java/org/example/MainJoueur.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class MainJoueur {
 
@@ -28,12 +29,22 @@ public class MainJoueur {
 
     public String evaluerMain() {
 
+        if (estUneCouleur() && estUneSuite()) {
+            return "Quinte Flush";
+        }
+
+        if (estUneCouleur()) {
+            return "Couleur (Flush)";
+        }
+
+        if (estUneSuite()) {
+            return "Suite (Straight)";
+        }
+
         int compteur = 0;
 
         for (int i = 0; i < cartes.size(); i++) {
             for (int j = i + 1; j < cartes.size(); j++) {
-
-                // Si deux cartes ont la même valeur = PAIRE
                 if (cartes.get(i).getValeur().equals(cartes.get(j).getValeur())) {
                     compteur++;
                 }
@@ -42,12 +53,62 @@ public class MainJoueur {
 
         if (compteur == 1)
             return "Paire";
+        else if (compteur == 2)
+            return "Deux paires";
         else if (compteur == 3)
-            return "Double paire";
+            return "Brelan";
         else if (compteur == 6)
-            return "Brelan ou mieux";
+            return "Carré";
 
         return "Aucune combinaison";
+    }
+
+
+    // Vérifier s'il y a une couleur (Flush)
+    public boolean estUneCouleur() {
+
+        if (cartes.size() != 5) return false;
+
+        String sorteDeReference = cartes.get(0).getSorte();
+
+        for (Carte c : cartes) {
+            if (!c.getSorte().equals(sorteDeReference)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    // Vérifier si les cartes forment une suite
+    public boolean estUneSuite() {
+        if (cartes.size() != 5) return false;
+
+        // Transformer les valeurs en nombres
+        ArrayList<Integer> valeurs = new ArrayList<>();
+
+        for (Carte c : cartes) {
+            valeurs.add(convertirValeur(c.getValeur()));
+        }
+
+        Collections.sort(valeurs);
+
+        for (int i = 0; i < valeurs.size() - 1; i++) {
+            if (valeurs.get(i + 1) - valeurs.get(i) != 1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private int convertirValeur(String v) {
+        return switch (v) {
+            case "A" -> 14;
+            case "R" -> 13;
+            case "D" -> 12;
+            case "V" -> 11;
+            default -> Integer.parseInt(v);
+        };
     }
 
 }
